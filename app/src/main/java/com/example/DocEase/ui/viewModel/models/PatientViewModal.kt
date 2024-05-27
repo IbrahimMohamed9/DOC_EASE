9da03902ec877194e24b11c91189c994b4d85735
IbrahimMoatazMohamed <ibrahim.mohamed@stu.ibu.edu.ba>
2024-05-27 21:47:00 +0200

    finish profile backend

--- app/src/main/java/com/example/DocEase/ui/viewModel/models/PatientViewModal.kt
+++ app/src/main/java/com/example/DocEase/ui/viewModel/models/PatientViewModal.kt
@@ -1,15 +1,18 @@
 package com.example.DocEase.ui.viewModel.models
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.DocEase.model.enums.PatientStatus
 import com.example.DocEase.model.models.Gender
 import com.example.DocEase.model.models.Patients
 import java.time.LocalDate
 
+@RequiresApi(Build.VERSION_CODES.O)
 data class PatientsDetails(
     val patientId: Int = 0,
     val name: String = "",
     val surname: String = "",
-    val DOJ: String = "${currentTime.dayOfMonth}-${currentTime.monthValue}-${currentTime.year}", //date of join
+    val DOJ: String = "${LocalDate.now().dayOfMonth}-${LocalDate.now().monthValue}-${LocalDate.now().year}", //date of join
     val DOB: String = "", //date of birth
     val phoneNumber: String = "",
     val email: String = "",
@@ -19,12 +22,13 @@ data class PatientsDetails(
     val status: PatientStatus = PatientStatus.STABLE
 )
 
-
-data class PatientsUiState(
+@RequiresApi(Build.VERSION_CODES.O)
+data class PatientsUiState  constructor(
     val patientsDetails: PatientsDetails = PatientsDetails(),
     val isEntryValid: Boolean = false
 )
 
+@RequiresApi(Build.VERSION_CODES.O)
 fun PatientsDetails.toPatients(): Patients = Patients(
     patientId = patientId,
     name = name,
@@ -39,6 +43,7 @@ fun PatientsDetails.toPatients(): Patients = Patients(
     password = password
 )
 
+@RequiresApi(Build.VERSION_CODES.O)
 fun Patients.toPatientsDetails() = PatientsDetails(
     patientId = patientId,
     name = name,
@@ -53,6 +58,7 @@ fun Patients.toPatientsDetails() = PatientsDetails(
     password = password
 )
 
+@RequiresApi(Build.VERSION_CODES.O)
 fun Patients.toPatientUiState(isEntryValid: Boolean = false):
         PatientsUiState = PatientsUiState(
     patientsDetails = this.toPatientsDetails(),
